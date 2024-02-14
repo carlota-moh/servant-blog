7dc5ee51b7cb4c783db37f5296fd9632fcfcece9
Charly <cmonedero@stackbuilders.com>
2024-02-14 13:05:10 +0100

    Get all users and users by id

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,40 +1,64 @@
 {-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveGeneric   #-}
 {-# LANGUAGE TypeOperators   #-}
-module Lib
-    ( startApp
-    , app
-    ) where
 
-import Data.Aeson
-import Data.Aeson.TH
-import Network.Wai
-import Network.Wai.Handler.Warp
-import Servant
+module Lib
+  ( startApp
+  , myApp
+  ) where
+
+import           Data.Aeson
+import           Data.Foldable            (find)
+import           GHC.Generics
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Servant
 
 data User = User
-  { userId        :: Int
-  , userFirstName :: String
-  , userLastName  :: String
-  } deriving (Eq, Show)
+  { _userId       :: Int
+  , _userName     :: String
+  , _userPassword :: String
+  } deriving (Generic, Eq, Show)
 
-$(deriveJSON defaultOptions ''User)
+instance ToJSON User where
+  toEncoding = genericToEncoding defaultOptions
 
-type API = "users" :> Get '[JSON] [User]
+instance FromJSON User
+
+type UsersAPI
+  = "users" :> "list-all" :> Get '[ JSON] [User] :<|> "users" :> Capture
+      "usrId"
+      Int :> Get '[ JSON] User
+
+myUsers :: [User]
+myUsers = [User 0 "charly" "1234", User 1 "gon" "5678"]
+
+-- TODO: move to utils
+getUserById :: Int -> Maybe User
+getUserById usrId = find (\user -> _userId user == usrId) myUsers
+
+checkUser :: Maybe User -> User
+checkUser mbUser =
+  case mbUser of
+    Nothing -> User (-1) "invalid" ""
+    Just u  -> u
+
+-- TODO: change this to connect to DB
+myServer :: Server UsersAPI
+myServer = allUsers :<|> oneUser
+  where
+    allUsers = return myUsers
+    oneUser :: Int -> Handler User
+    oneUser = return . checkUser . getUserById
+
+-- boilerplate
+myApi :: Proxy UsersAPI
+myApi = Proxy
+
+myApp :: Application
+myApp = serve myApi myServer
 
 startApp :: IO ()
-startApp = run 8080 app
-
-app :: Application
-app = serve api server
-
-api :: Proxy API
-api = Proxy
-
-server :: Server API
-server = return users
-
-users :: [User]
-users = [ User 1 "Isaac" "Newton"
-        , User 2 "Albert" "Einstein"
-        ]
+startApp = do 
+  putStrLn "Running server on http://localhost:8080"
+  run 8080 myApp
