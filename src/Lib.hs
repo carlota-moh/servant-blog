02318296e6454bc9726316daf46c4e96b3cda0d9
Charly <cmonedero@stackbuilders.com>
2024-02-19 19:13:58 +0100

    Delete user endpoint

--- src/Lib.hs
+++ src/Lib.hs
@@ -30,7 +30,7 @@ type UsersAPI
       "usrId"
       Int :> Get '[ JSON] User :<|> "createUser" :> ReqBody '[ JSON] User :> Post
       '[ JSON]
-      User
+      User :<|> "deleteUser" :> Capture "usrId" Int :> Delete '[ JSON] [User]
 
 myUsers :: [User]
 myUsers = [User 0 "charly" "1234", User 1 "gon" "5678"]
@@ -39,21 +39,28 @@ myUsers = [User 0 "charly" "1234", User 1 "gon" "5678"]
 getUserById :: Int -> Maybe User
 getUserById usrId = find (\user -> userId user == usrId) myUsers
 
+-- TODO: Improve error handling
 checkUser :: Maybe User -> User
 checkUser mbUser =
   case mbUser of
     Nothing -> User (-1) "invalid" ""
     Just u  -> u
 
+-- TODO: move to utils
+deleteUserFromList :: Int -> [User]
+deleteUserFromList uid = filter (\user -> userId user /= uid) myUsers
+
 -- TODO: change this to connect to DB
 myServer :: Server UsersAPI
-myServer = allUsers :<|> oneUser :<|> createUser
+myServer = allUsers :<|> oneUser :<|> createUser :<|> deleteUser
   where
     allUsers = return myUsers
     oneUser :: Int -> Handler User
     oneUser = return . checkUser . getUserById
     createUser :: User -> Handler User
     createUser = return
+    deleteUser :: Int -> Handler [User]
+    deleteUser = return . deleteUserFromList
 
 -- boilerplate
 myApi :: Proxy UsersAPI
